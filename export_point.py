badff4fa6015ac0bc1ec2119f435e7e6fe920b75
indridi <indridi@kvikna.com>
2022-02-22 00:35:03 +0000

    export constituents to json - calculate using javascript

--- export_point.py
+++ export_point.py
@@ -2,15 +2,20 @@ import netCDF4 as nc
 from scipy import interpolate
 
 from pathlib import Path
+import json
 
 dir = 'ocean_tide_extrapolated'
 
 pathlist = Path(dir).glob('**/*.nc')
-point = (63, 23)
+#point = (63, 23)
+# Reykjavík : 
+point = (64.166535, -21.972971+360)
+
+data = []
 for path in pathlist:
     constname = path.stem
     if constname == 'la2':
-        constname = 'lambda2'
+        constname = 'lam2'
 
     ds = nc.Dataset(path)
     phase = interpolate.interpn((ds.variables['lat'], ds.variables['lon']), ds.variables['phase'], point ) [0]
@@ -18,4 +23,8 @@ for path in pathlist:
     ampl = interpolate.interpn((ds.variables['lat'], ds.variables['lon']), ds.variables['amplitude'], point )[0]
 
     print(f' {constname} {ampl}, {phase}')
+    data.append({'name':constname.upper(), 'amplitude':ampl, 'phase':phase})
 
+
+with open('rvk.json', 'w') as f:
+    json.dump(data, f)
